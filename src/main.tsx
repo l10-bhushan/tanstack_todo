72b41c054048026b45278d633f3b540f3bd40a04
Bhushan <bhushan.rajput.l1odevs@gmail.com>
2026-04-20 15:58:24 +0530

    Setup of tanstack query and axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDom from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// This enables two things
+// Caching
+// Global query control
+const queryClient = new QueryClient();
+
+ReactDom.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
-)
+  </QueryClientProvider>,
+);
